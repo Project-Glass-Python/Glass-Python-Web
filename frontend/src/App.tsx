7a0dcf9a5e0280a3dfd2f1f0e057329cf95229a9
Anthony Formichelli <formichelli.7@osu.edu>
2024-02-24 12:18:35 -0500

    routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Home from "./Home/Home";
+import Products from "./Products/Products";
+import { Link, Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 function App() {
   const openMenu = () => {
@@ -11,84 +14,41 @@ function App() {
     document.querySelector(".sidebar")?.classList.remove("open");
   };
   return (
-    <div className="grid-container">
-      <header className="header">
-        <div className="brand">
-          <button onClick={openMenu}>&#9776;</button>
-          <a href="index.html">Glass Python</a>
-        </div>
-        <div className="header-links">
-          <a href="cart.html">Cart</a>
-          <a href="signin.html">Sign In</a>
-        </div>
-      </header>
-      <aside className="sidebar">
-        <h3>Shopping Categories</h3>
-        <button className="sidebar-close-button" onClick={closeMenu}>
-          X
-        </button>
-        <ul>
-          <li>
-            <a href="index.html">Pants</a>
-          </li>
-          <li>
-            <a href="index.html">Shirts</a>
-          </li>
-        </ul>
-      </aside>
-      <main className="main">
-        <div className="content">
+    <Router>
+      <div className="grid-container">
+        <header className="header">
+          <div className="brand">
+            <button onClick={openMenu}>&#9776;</button>
+            <a href="index.html">Glass Python</a>
+          </div>
+          <div className="header-links">
+            <Link to="/">Home</Link>
+            <Link to="/catalog">Catalog</Link>
+          </div>
+        </header>
+        <aside className="sidebar">
+          <h3>Shopping Categories</h3>
+          <button className="sidebar-close-button" onClick={closeMenu}>
+            X
+          </button>
           <ul>
-            <li className="product">
-              <div className="product">
-                <img
-                  className="product-image"
-                  src="images/d1.jpg"
-                  alt="product"
-                />
-                <div className="product-name">
-                  <a href="product.html">Slim Shirt</a>
-                </div>
-                <div className="product-brand">Nike</div>
-                <div className="product-price">$60</div>
-                <div className="product-rating">4.5 Stars (10 Reviews)</div>
-              </div>
+            <li>
+              <a href="index.html">Pants</a>
             </li>
-            <li className="product">
-              <div className="product">
-                <img
-                  className="product-image"
-                  src="images/d1.jpg"
-                  alt="product"
-                />
-                <div className="product-name">
-                  <a href="product.html">Slim Shirt</a>
-                </div>
-                <div className="product-brand">Nike</div>
-                <div className="product-price">$60</div>
-                <div className="product-rating">4.5 Stars (10 Reviews)</div>
-              </div>
-            </li>
-            <li className="product">
-              <div className="product">
-                <img
-                  className="product-image"
-                  src="images/d1.jpg"
-                  alt="product"
-                />
-                <div className="product-name">
-                  <a href="product.html">Slim Shirt</a>
-                </div>
-                <div className="product-brand">Nike</div>
-                <div className="product-price">$60</div>
-                <div className="product-rating">4.5 Stars (10 Reviews)</div>
-              </div>
+            <li>
+              <a href="index.html">Shirts</a>
             </li>
           </ul>
-        </div>
-      </main>
-      <footer className="footer">&copy; 2024 Glass Python</footer>
-    </div>
+        </aside>
+        <main className="main">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/catalog" element={<Products />} />
+          </Routes>
+        </main>
+        <footer className="footer">&copy; 2024 Glass Python</footer>
+      </div>
+    </Router>
   );
 }
 
